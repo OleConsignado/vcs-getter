20ea07b7613db190ac10e0b998da615b0acca908
Matheus Neder <matheusneder@gmail.com>
2018-03-08 16:38:02 -0300

    Fixed optional configuration

--- VCSGetterConf.js
+++ VCSGetterConf.js
@@ -3,11 +3,11 @@ const { TFSConf, TFSCollection } = require("./tfs");
 
 
 class VCSGetterConf {
-	constructor({ git, tfs }) {
+	constructor({ git, tfs } = {}) {
 		this.git = new GitTreeConf(git);
 		this.tfs = new TFSConf(tfs);
 		this.tfs.collections = [];
-		
+
 		if(tfs && tfs.collections) {
 			for(let collection of tfs.collections) {
 				this.tfs.collections.push(new TFSCollection(collection));
